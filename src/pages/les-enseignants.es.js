d170433a757df81e7caec161aa5ad7cdd5cae820
PLF63 <35199429+PLF63@users.noreply.github.com>
2018-01-28 13:05:01 +0100

    Update les-enseignants.es.js
    contenu catalan OK

--- src/pages/les-enseignants.es.js
+++ src/pages/les-enseignants.es.js
@@ -6,47 +6,27 @@ import {Link, InnerLink} from "../components/base";
 import TeacherList from "../components/teacherList";
 
 const Teachers = () => (
-    <Page title="Le Capriccio Français and Academy faculty">
+    <Page title="Le Capriccio Français i els ensenyants de l’Acadèmia">
         <Lead>
             <p>
-                Founded by <InnerLink to="/en/philippe-le-fevre">Philippe Le Fèvre</InnerLink>, Le
-                Capriccio Français has performed hundreds of times, in prestigious and famous
-                festivals as well as in more humble places. We have performed with the most renowned
-                soloists and with amateur choirs alike. Our public may be music lovers or students
-                in disadvantaged neighborhoods. Our goal is always the same: to perform with
-                determination and passion.
-            </p>
+                Le Capriccio Français va celebrar el seu 20è aniversari el 2015. Des de la seva creació ha donat diversos 
+    centenars de concerts. Si bé el seu repertori essencial ens porta a la música del segle XVIIIè, regularment interpreten 
+    repertori clàssic i romàntic: sense cap dubte que la seva Traviata amb instruments d’època va ser una estrena mundial!
+</p>
+            <p>Le Capriccio Français alterna produccions de gran envergadura (Passió segons Sant Joan de J.S. Bach, 
+    Israel en Egipte de Händel) i produccions més intimistes. Després de <Link href="https://www.youtube.com/watch?v=Hb4y2qL6MkM">Mozart, opéra intime  </Link>{" "}
+                –{" "} –  <Link href="https://www.youtube.com/watch?v=02E8FGrew-E">Haendel, caro Sassone</Link>{" "}
+                –{" "} – 
+ <Link href="https://www.youtube.com/watch?v=OmvK1gjEx_E">Vivaldi, le vénitien</Link>{" "}, le Capriccio Français ha creat el seu darrer espectacle Si Versailles m'était chanté, 
+dedicat a l’òpera francesa dels segles XVIIè i XVIIIè. 
+</p>
             <p>
-                When Le Capriccio Français was founded, we intended to perform the 18th century
-                amazing repertoire. We play period instruments because we mean to link every musical
-                era with its proper instruments and interpretation. But we have since approached
-                classical and romantic repertoires, and our performance of the Traviata on period
-                instruments was a world premiere!
+Sempre actius pedagògicament, entre l’Acadèmia d’estiu i accions culturals a ciutats, le Capriccio Français realitza 
+també accions de prestigi com la seva residència al Festival de Froville, en el marc de la qual Philippe Le Fèvre ha 
+convidat nombrosos artistes (Gérad Lesne, Maarten Koningsberger, Andréas Scholl) a venir a treballar amb ell per formar 
+joves artistes.
             </p>
-            <p>
-                Le Capriccio Français could lately be heard in great performances (J.S. Bach Mass in
-                B minor, St. Matthew Passion and St John Passion, W.A. Mozart Great Mass in C minor)
-                and more intimate concerts, as well as our four productions:{" "}
-                <Link href="https://www.youtube.com/watch?v=Hb4y2qL6MkM">
-                    Mozart, an intimate opera
-                </Link>{" "}
-                –{" "}
-                <Link href="https://www.youtube.com/watch?v=02E8FGrew-E">
-                    Haendel, caro Sassone
-                </Link>{" "}
-                –{" "}
-                <Link href="https://www.youtube.com/watch?v=OmvK1gjEx_E">
-                    Vivaldi, the Venetian
-                </Link>{" "}
-                - Si Versailles m'était chanté.
-            </p>
-            <p>
-                We also emphasize accompaniment and transmission: cultural actions such as school
-                programs, our summer music Academy, but also prestigious actions such as our
-                residence at the Froville Festival, where Philippe Le Fèvre has invited numerous
-                artists (Gérard Lesne, Maarten Koningsberger, Andréas Scholl) to offer top-level
-                masterclasses to young artists!
-            </p>
+           
         </Lead>
         <TeacherList language="en" />
     </Page>
