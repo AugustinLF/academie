8cca7b969c0acc95b3b93debfcdaad51457185f2
PLF63 <35199429+PLF63@users.noreply.github.com>
2018-01-28 13:13:41 +0100

    Update philippe-le-fevre.es.js
    contenu catalan OK

--- src/pages/philippe-le-fevre.es.js
+++ src/pages/philippe-le-fevre.es.js
@@ -7,52 +7,31 @@ import philippe from "../philippe.jpg";
 const Philippe = () => (
     <InnerPage
         name="Philippe Le Fèvre"
-        title="Choir and choir conducting"
+        title="Cor i Direcció coral"
         imgUrl={philippe}
         content={
             <div>
                 <p>
-                    As a musical director and as a choral director, Philippe Le Fèvre is eager to
-                    make classical music accessible to as large an audience as possible. When he
-                    performs with Le Capriccio Français or creates a symphonic orchestra for
-                    conservatory students, he has a constant goal: to help people hear and perform
-                    music.
-                </p>
+                    Director d’orquestra i de cor, Philippe Le Fèvre treballa perquè la música pugui arribar al més alt estadi.
+    Sigui com a cap del Capriccio Français o creant una orquestra simfònica d’alumnes per una xarxa de conservatoris, 
+    el seu objectiu és constant: fer interpretar i fer sentir la música.
+ </p>
                 <p>
-                    Alongside his work as a musical director, Philippe Le Fèvre favors pedagogy and
-                    transmission: he has therefore founded a Summer Music Academy for future young
-                    artists, he is a Professor at Saint-Maur Regional Conservatory, Cergy Regional
-                    Conservatory and Paris 9e Conservatory. He is also the Choral conductor of three
-                    amateur choirs including the french Parliament choir, and he regularly delivers
-                    lectures.
-                </p>
+És per això que al costat de la seva activitat de director, afegeix donar una importància gran a la pedagogia i a la 
+    transmissió de coneixements: ha creat per això una acadèmia d’estiu des d’on forma els artistes joves del demà, ensenya 
+    al Conservatoire à Rayonnement Régional de Saint-Maur, de Cergy-Pontoise i al del 9è barri de Paris. 
+    Dirigeix cors amateurs on ajuda a fer-los descobrir obres capdals i obres desconegudes i dóna conferències per a 
+    donar claus als melòmans.
+ </p>
                 <p>
-                    The same passion has led him to join the{" "}
-                    <Link href="https://artchoral.org/">French Choral Art Institute</Link> in 2010,
-                    as an Artistic and Pedagogical Director for several years, and now as a member
-                    of its Board. He aims at creating new repertoires, developing choral directing
-                    programs, and establishing international connections.
-                </p>
+És per això que després d’haver estat director artístic i pedagògic de l’Institut Français d’Art Choral durant diversos anys, 
+    ha aconseguit prop del consell d’administració de treballar sobre nous repertoris, sobre la formació de directors de cor 
+    i sobre la creació de xarxes internacionals.
+</p>
                 <p>
-                    Philippe Le Fèvre enjoys following unusual paths : he has recently taken part in
-                    the movie “Intouchables”, together with Le Capriccio Français, he likes to
-                    combine different musical genres : a classical orchestra performing with a gipsy
-                    jazz band or with piano and bandoneon, and he regularly performs French and
-                    Russian repertoires with Russian professional choirs.
-                </p>
-                <p>
-                    Philippe Le Fèvre cherishes the Baroque definition of the word “emotion”: that
-                    which makes you move and makes you different. His inner aspiration is to make
-                    his audience feel happy for sharing these ravishing musical moments.
-                </p>
-                <p>
-                    Philippe Le Fèvre hosts the{" "}
-                    <InnerLink to="/en/stage-direction-de-choeur">
-                        choir conducting academy
-                    </InnerLink>{" "}
-                    and shares the choir conductin with{" "}
-                    <InnerLink to="/en/josep-cabre">Josep Cabré</InnerLink> for the{" "}
-                    <InnerLink to="/en/stage-choral">vocal academy</InnerLink>
+Philippe Le Fèvre, degut a la seva passió per la música ha escollit de fer-se seva la definició del mot emoció en temps 
+    del barroc: és el que fa moure els individus i els torna diferents. I contribuir, encara que sigui d’una manera senzilla 
+    a fer feliços als que han compartit amb ell grans moments musicals.
                 </p>
             </div>
         }
