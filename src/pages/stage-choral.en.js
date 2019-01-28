dfc218c50df63759134817f6b6f094364dfe2101
PLF63 <35199429+PLF63@users.noreply.github.com>
2019-01-28 15:55:49 +0100

    Update stage-choral.en.js

--- src/pages/stage-choral.en.js
+++ src/pages/stage-choral.en.js
@@ -40,6 +40,45 @@ const Choral = () => (
                     therefore helps the choral singers improve their skills with as many vocal
                     technique lessons as possible.
                 </p>
+     <p>
+                   
+                    <InnerLink to="/fr/philippe-le-fevre">Philippe Le Fèvre</InnerLink>Philippe Le Fèvre will direct the first 
+    part of the choral program, dedicated to works by Tomas Luis de Victoria (1548-1611). Victoria is one of the greatest 
+Spanish composers and was one of the most prolific European composers of the counter-reform. 
+He spent nearly 20 years in Rome, where he studied, and prepared for the priesthood. He succeeded Palestrina as chapel master 
+of the Roman Seminary. Victoria subsequently returned to Madrid to become the director of music and organist of two 
+empresses. Immediate renown followed the publication in 1585 in Rome of a collection of nine lamentations, 
+    eighteen responses of darkness and two passions, composed for Holy Week
+                </p>
+                <p>
+                    <List>
+                        <ListItem>Responses: Eram quasi agnus</ListItem>
+                        <ListItem>Lamentation: Lamed. Matribus suis dixerunt</ListItem>
+                        <ListItem>Responses: Caligaverunt Oculi mei</ListItem>
+                        <ListItem>Lamentation: Aleph. Quomodo obscuratum</ListItem>
+                        <ListItem>Responses: Sepulto Domino</ListItem>
+                    </List>
+                </p>
+                <p>
+                    <InnerLink to="/en/adam-vidovic">Adam Vidovic</InnerLink> Adam Vidovic will work on the Anglican tradition 
+of choral Evensong.  Sung in the late afternoon or early evening, the service has remained virtually unchanged within British 
+cathedrals and the chapels of Oxford and Cambridge University, since the last version of the Book of Common Prayer (1662). 
+These institutions continue to perpetuate this wonderful tradition today, and draw from a wealth of high-quality choral 
+(and organ) repertoire, composed throughout the ages. Without respecting any particular liturgical season, we will sing a 
+selection of these works, including anthems, hymns and responses.
+                </p>
+                <p>
+                    <List>
+                        <ListItem>Introït If Ye Love Me - Thomas Tallis </ListItem>
+                        <ListItem>The Preces - William Smith</ListItem>
+                        <ListItem>Hymn - George Frideric Händel - Sinners obey the Gospel Word</ListItem>
+                        <ListItem>Magnificat – Henry Purcell</ListItem>
+                        <ListItem>Nunc Dimitis – Henry Purcell</ListItem>
+                        <ListItem>Anthem – I was Glad Henry Purcell</ListItem>
+                        <ListItem>Hymne - Rejoice the Lord is King Händel</ListItem>
+                        <ListItem>Bénédiction & Amen - Gibbons - Threefold Amen</ListItem>
+                    </List>
+</p>
                 <p>
                     Choral singers must study the whole program before the beginning of the
                     workshop. Downloadable choral scores are provided by mail, free of charge. And
