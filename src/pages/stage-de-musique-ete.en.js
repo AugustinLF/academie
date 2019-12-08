0f82d6c6bcac561c5f39718bd2738d8024918216
PLF63 <35199429+PLF63@users.noreply.github.com>
2019-12-08 20:28:21 +0100

    Update stage-de-musique-ete.en.js

--- src/pages/stage-de-musique-ete.en.js
+++ src/pages/stage-de-musique-ete.en.js
@@ -5,12 +5,12 @@ import Lead from "../components/lead";
 import ProgrammeList from "../components/programmeList";
 
 const Programme = () => (
-    <Page title="Summer Music Academy 2019">
+    <Page title="Summer Music Academy 2020">
         <Lead>
             <p>
                 Le Capriccio Français Summer Music Academy offers a tuition program for singers,
                 harpsichordists, and students in choir conducting, as well as a choral program, from
-                July 21 (arrival before dinner) to July 28 (departure July 29 after breakfast). The
+                July 19 (arrival before dinner) to July 26 (departure July 27 after breakfast). The
                 Academy is situated in Barcelona, in Casa Sant Felip Neri, an ancient convent
                 welcoming meetings, and especially musical workshops.
             </p>
@@ -18,7 +18,7 @@ const Programme = () => (
                 The students will develop their technical skills together with their stylistic
                 skills on early music repertoire. Singers, instrumentalists and choral singers work
                 together all through the session, sharing their stylistic knowledge, and prepare
-                auditions and the final concert together. The concert will be held on Sunday 28 July
+                auditions and the final concert together. The concert will be held on Sunday 26 July
                 (5:00 pm) in Església de Sant Gaietà.
             </p>
             <p>
