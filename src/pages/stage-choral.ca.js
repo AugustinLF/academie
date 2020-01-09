1174111900f36cebd9b90b5f16c4917792f48c4f
PLF63 <35199429+PLF63@users.noreply.github.com>
2020-01-09 18:13:00 +0100

    Update stage-choral.ca.js

--- src/pages/stage-choral.ca.js
+++ src/pages/stage-choral.ca.js
@@ -23,7 +23,28 @@ const Choral = () => (
                     En parallel als assajos, els cantaires rebran classes de tècnica vocal de{" "}
                     <InnerLink to="/ca/muriel-ferraro">Muriel Ferraro</InnerLink>
                 </p>
-               
+   <p>El programa del cor:</p>             
+    <p>
+        <List>
+        <ListItem>Wycombe : Summer Is Icumen</ListItem>
+        <ListItem>William Byrd : The Eagle's Force subdues each Bird that flies (SAH)</ListItem>					
+        <ListItem>William Byrd : Compel the Hawk (SSATB)</ListItem>
+        <ListItem>Thomas Morley : Fly Love that art so Sprightly (SSATB)</ListItem>
+        <ListItem>Thomas Weelkes : The Nightingale (SAH)</ListItem>
+        <ListItem>Thomas Vautor : Sweet Sufflok Owl (SSATB)</ListItem>
+        <ListItem>John Bartlett : Of All The Birds That I Do Know (SATB) </ListItem>
+                </p>
+                <p>
+                    With{" "}<InnerLink to="/fr/philippe-le-fevre">Philippe Le Fèvre</InnerLink>:
+ </p>
+                <p>
+        <List>
+        <ListItem>Marc-Antoine Charpentier : Regina Coeli H46 (SATB)</ListItem>				
+        <ListItem>Guillaume Bouzignac : O mors, ero mors (SSATB)</ListItem>			  	
+        <ListItem>Marc-Antoine Charpentier : Alma redemptoris Mater H44	(SATB)</ListItem>	
+        <ListItem>Guillaume Bouzignac : Ha, Plange Filia Jerusalem (SSATB)</ListItem>		
+        <ListItem>Marc-Antoine Charpentier : Transfige dulcissime Jesu H251	(SSATB)</ListItem>	
+ </p>
             </div>
         }
     />
