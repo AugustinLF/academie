55c3724d0ec513999d641528f5bfdffdc938d7e6
PLF63 <35199429+PLF63@users.noreply.github.com>
2020-05-02 16:26:12 +0200

    Update stage-direction-de-choeur.fr.js

--- src/pages/stage-direction-de-choeur.fr.js
+++ src/pages/stage-direction-de-choeur.fr.js
@@ -13,9 +13,8 @@ const ChoirConducting = (props) => (
                 <div>
                     <p>
                         Ce stage s'adresse à des étudiants inscrits dans un cursus de direction de
-                        chœur et souhaitant aborder toutes les facettes du métier de chef de chœur,
-                        et travailler quotidiennement avec un chœur école. Il est aussi destiné à
-                        des chefs de chœur amateurs ou encore à des musiciens professionnels
+                        chœur et souhaitant aborder toutes les facettes du métier de chef de chœur. 
+                        Il est aussi destiné à des chefs de chœur amateurs ou encore à des musiciens professionnels
                         (professeurs de formation musicale, professeurs de musique dans les lycées
                         et collèges, musiciens intervenants, etc.).
                     </p>
@@ -24,6 +23,7 @@ const ChoirConducting = (props) => (
                         s'articulera entre travail à la table fait en commun, gestique, conduite
                         d'une répétition, travail avec les choristes.
                     </p>
+
                 </div>
             }
         />
